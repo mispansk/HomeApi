5bc2ce6764f53ec26e4af60727497f0abd7f6148
mispansk <mispansk@gmail.com>
2023-03-28 00:09:03 +0700

    Добавьте файлы проекта.

---
+++ HomeApi.Contracts/Models/Rooms/EditRoomRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Contracts.Models.Rooms
+{
+    /// <summary>
+    /// Запрос для обновления свойств подключенной комнаты
+    /// </summary>
+    public class EditRoomRequest
+    {
+        public int NewArea { get; set; }
+        public bool NewGasConnected { get; set; }
+        public int NewVoltage { get; set; }
+    }
+}
---
+++ HomeApi.Data/Queries/UpdateRoomQuery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeApi.Data.Queries
+{
+
+    /// <summary>
+    /// Класс для передачи дополнительных параметров при обновлении свойств комнаты 
+    /// </summary>
+    public class UpdateRoomQuery
+    {
+        //public string NewName { get; }
+        public int NewArea { get; set; }
+        public bool NewGasConnected { get; set; }
+        public int NewVoltage { get; set; }
+
+        public UpdateRoomQuery(int newArea = 0, bool newGasConnected = false, int newVoltage = 0)
+        {
+            NewArea = newArea;
+            NewGasConnected = newGasConnected;
+            NewVoltage = newVoltage;
+        }
+    }
+}
